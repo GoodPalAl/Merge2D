673ff2e23a3b8b41462ff435082cf1917e89552c
GoodPalAl <insanimegamer@gmail.com>
2024-07-12 00:10:47 -0400

    First Commit
    Item position can be controlled by mouse position and dropped on left mouse click.

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
 
+[RequireComponent (typeof(Rigidbody2D))]
 public class ItemController : MonoBehaviour
 {
     Vector3 DropPosition;
-    float DropHeight = 4f;
     bool HasBeenDropped = false;
+
+    [SerializeField]
+    float DropHeight = 4f;
+    [SerializeField]
+    float MapBorderLeft = -2.75f;
+    [SerializeField]
+    float MapBorderRight = 2.75f;
+    [SerializeField]
+    float ItemRadius = 0.1f;
 
     // Update is called once per frame
     void Update()
@@ -17,11 +26,17 @@ public class ItemController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // If the item is being held and the left-mouse button is pressed, drop the item.
         if (!HasBeenDropped && Input.GetMouseButtonDown(0))
         {
             DropItem();
         }
-        
+        // If the right-mouse button is pressed, restart the dropped item. DEBUG PURPOSES.
+        if (Input.GetMouseButtonDown(1))
+        {
+            Restart();
+        }
+
     }
 
     void FollowMouse()
@@ -32,16 +47,35 @@ public class ItemController : MonoBehaviour
         //  <Screen.width, Screen.height, 0> = Top-Right of screen
         Vector3 WorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        // Keep item in border of map.
+        if (WorldPos.x > MapBorderRight - ItemRadius) WorldPos.x = MapBorderRight - ItemRadius;
+        if (WorldPos.x < MapBorderLeft + ItemRadius) WorldPos.x = MapBorderLeft + ItemRadius;
+
+        // Set item transform to appropriate position:
+        // x = x of mouse curser within map boundaries,
+        // y = height of map,
+        // z = distance of near clipping plane from Camera
         DropPosition = new Vector3(WorldPos.x, DropHeight, Camera.main.nearClipPlane);
-
-
         transform.position = DropPosition;
+
+        // Freeze rotation on z-axis.
+        GetComponent<Rigidbody2D>().freezeRotation = true;
     }
 
     void DropItem()
     {
+        // Flag that the item has been dropped.
         HasBeenDropped = true;
+        // Unfreeze rotation on z-axis.
+        GetComponent<Rigidbody2D>().freezeRotation = false;
 
         // Spawn a new item
+    }
+
+    private void Restart()
+    {
+        // Flag that the item was "picked back up".
+        HasBeenDropped = false;
+        FollowMouse();
     }
 }
